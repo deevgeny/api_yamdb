4b542f6c3256290bf403a6f9b1907fd2f5bf5e41
Evgeny <evgenyderiglazov@yandex.ru>
2022-07-31 15:21:56 +0300

    Create: review and comment models, register in admin

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,30 +1,65 @@
 from django.contrib import admin
 
-from reviews.models import Category, Genre, Title
+from .models import Category, Comment, Genre, Review, Title
 
 
 @admin.register(Category)
 class CategoriesAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'slug',)  # какие поля отображаются
-    list_filter = ('name',)  # какие поля отображаются справа
-    search_fields = ('name',)  # поиск
-    list_display_links = ('name',)  # на какое поле можно кликнуть и зайти
-    prepopulated_fields = {'slug': ('name',)}  # формирование слага из имени
+    list_display = (
+        "id",
+        "name",
+        "slug",
+    )
+    list_filter = ("name",)
+    search_fields = ("name",)
+    list_display_links = ("name",)
+    prepopulated_fields = {"slug": ("name",)}
 
 
 @admin.register(Genre)
 class GenresAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'slug',)
-    list_filter = ('name',)
-    search_fields = ('name',)
-    list_display_links = ('name',)
-    prepopulated_fields = {'slug': ('name',)}
+    list_display = (
+        "id",
+        "name",
+        "slug",
+    )
+    list_filter = ("name",)
+    search_fields = ("name",)
+    list_display_links = ("name",)
+    prepopulated_fields = {"slug": ("name",)}
 
 
 @admin.register(Title)
 class TitleAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'year', 'description',)
-    list_filter = ('genre', 'category', 'year',)
-    search_fields = ('name', 'year', 'genre', 'category',)
-    list_display_links = ('name', 'year',)
-    empty_value_display = '-пусто-'  # заместо NULL
+    list_display = (
+        "id",
+        "name",
+        "year",
+        "description",
+    )
+    list_filter = (
+        "genre",
+        "category",
+        "year",
+    )
+    search_fields = (
+        "name",
+        "year",
+        "genre",
+        "category",
+    )
+    list_display_links = (
+        "name",
+        "year",
+    )
+    empty_value_display = "-пусто-"
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    pass
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -6,23 +6,24 @@ User = get_user_model()
 
 class Category(models.Model):
     """Модель категории."""
+
     name = models.CharField(
         max_length=256,
         verbose_name="Название категории",
-        help_text="Добавьте название категории"
+        help_text="Добавьте название категории",
     )
     slug = models.SlugField(
         max_length=50,
         unique=True,
         verbose_name="Уникальный адрес категории",
-        help_text="Добавьте адрес категории"
+        help_text="Добавьте адрес категории",
     )
 
     class Meta:
         constraints = [
             models.UniqueConstraint(
-                name='exclude the re-creation of the сategory',
-                fields=['name', 'slug'],
+                name="exclude the re-creation of the сategory",
+                fields=["name", "slug"],
             )
         ]
         verbose_name = "Категория"
@@ -34,23 +35,24 @@ class Category(models.Model):
 
 class Genre(models.Model):
     """Модель жанра."""
+
     name = models.CharField(
         max_length=256,
         verbose_name="Название жанра",
-        help_text="Добавьте название жанра"
+        help_text="Добавьте название жанра",
     )
     slug = models.SlugField(
         max_length=50,
         unique=True,
         verbose_name="Уникальный адрес жанра",
-        help_text="Добавьте адрес жанра"
+        help_text="Добавьте адрес жанра",
     )
 
     class Meta:
         constraints = [
             models.UniqueConstraint(
-                name='exclude the re-creation of the genre',
-                fields=['name', 'slug'],
+                name="exclude the re-creation of the genre",
+                fields=["name", "slug"],
             )
         ]
         verbose_name = "Жанр"
@@ -62,42 +64,43 @@ class Genre(models.Model):
 
 class Title(models.Model):
     """Модель произведения."""
+
     name = models.CharField(
         max_length=256,
         verbose_name="Название произведения",
-        help_text="Добавьте название произведения"
+        help_text="Добавьте название произведения",
     )
     year = models.PositiveSmallIntegerField(
         verbose_name="Год выпуска произведения",
-        help_text="Добавьте год выпуска произведения"
+        help_text="Добавьте год выпуска произведения",
     )
     description = models.TextField(
         null=True,
         blank=True,
         verbose_name="Описание произведения",
-        help_text="Добавьте описание произведения"
+        help_text="Добавьте описание произведения",
     )
     genre = models.ManyToManyField(
         Genre,
-        related_name='titles',
+        related_name="titles",
         verbose_name="Жанр",
-        help_text='Выберите жанр'
+        help_text="Выберите жанр",
     )
     category = models.ForeignKey(
         Category,
         blank=True,
         null=True,
         on_delete=models.SET_NULL,
-        related_name='titles',
+        related_name="titles",
         verbose_name="Категория",
-        help_text="Выберите категорию"
+        help_text="Выберите категорию",
     )
 
     class Meta:
         constraints = [
             models.UniqueConstraint(
-                name='exclude the re-creation of the titles',
-                fields=['name', 'year'],
+                name="exclude the re-creation of the titles",
+                fields=["name", "year"],
             )
         ]
         verbose_name = "Произведение"
@@ -105,3 +108,84 @@ class Title(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Review(models.Model):
+    """Review model."""
+
+    RATING_CHOICES = [
+        (None, "---"),
+        (1, "1"),
+        (2, "2"),
+        (3, "3"),
+        (4, "4"),
+        (5, "5"),
+        (6, "6"),
+        (7, "7"),
+        (8, "8"),
+        (9, "9"),
+        (10, "10"),
+    ]
+
+    title_id = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE,
+        related_name="rating",
+        verbose_name="Отзыв",
+        help_text="Выберите произведение",
+    )
+    text = models.TextField(verbose_name="Текст отзыва")
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name="Автор",
+        related_name="review",
+    )
+    score = models.IntegerField(
+        verbose_name="Оценка",
+        help_text="Дайте оценку произведению от 1 до 10",
+        choices=RATING_CHOICES,
+        default="---",
+    )
+    pub_date = models.DateTimeField(
+        verbose_name="Дата публикации",
+        auto_now_add=True,
+    )
+
+    class Meta:
+        verbose_name = "Отзыв"
+        verbose_name_plural = "Отзывы"
+
+    def __str__(self):
+        return self.title_id
+
+
+class Comment(models.Model):
+    """Comment model."""
+
+    review_id = models.ForeignKey(
+        Review,
+        on_delete=models.CASCADE,
+        verbose_name="Обзор",
+    )
+    text = models.TextField(
+        verbose_name="Комментарий",
+        blank=False,
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name="Автор",
+        related_name="comment",
+    )
+    pub_date = models.DateTimeField(
+        verbose_name="Дата публикации",
+        auto_now_add=True,
+    )
+
+    class Meta:
+        verbose_name = "Комментарий к отзыву"
+        verbose_name_plural = "Комментарии к отзывам"
+
+    def __str__(self):
+        return self.review_id
